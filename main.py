0b256033904e86cb0ed0d328b18881eb0556109c
Luiz Gustavo Canzone Sikorski <canzoneluiz@gmail.com>
2026-05-19 12:13:43 -0300

    correção de alguns problemas do código original e adição do calculo da media
    Corrigi um problema no cadastro de aluno que antes aceitava nomes vazios, idades negativas e absurdas e notas fora do escopo de 0 a 10. Também adicionei uma nova função que calcula a media da turma coletando as notas na lista de cada aluno e dividindo pelo total de pessoas

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 sair = False
-alunos = [
-    ["Carlos", 17, 8.5],
-    ["Maria", 18, 9.0],
-    ["João", 16, 7.2]
-]
+alunos = []
 
 
 while sair == False:
@@ -13,24 +9,35 @@ Cadastrar aluno: 1
 Listar alunos: 2
 Buscar aluno: 3
 Remover aluno: 4
-Sair: 5''')
+media da turma: 5
+Sair: 6''')
     try:
         menu_escolha = int(input('O que deseja fazer? '))
 
-        if menu_escolha not in [1, 2, 3, 4, 5]:
+        if menu_escolha not in [1, 2, 3, 4, 5, 6]:
             print('escolha inválida!')
 
         elif menu_escolha == 1:
-            nome = input('Digite o nome do aluno: ')
-            idade = int(input('Digite a idade: '))
-            nota = float(input('Digite a nota final: '))
+            while True:
+                nome = input('Digite o nome do aluno: ').strip()
+                if nome != "":
+                    break
+                print('Nome vazio!Tente novamente!')
+            while True:
+                idade = int(input('Digite a idade: '))
+                if 0 < idade <= 100:
+                    break
+                print('idade invalida!Tente novamente!')
+            while True:
+                nota = float(input('Digite a nota final: '))
+                if 0 <= nota <= 10:
+                    break
+                print('nota invalida!Tente novamente!')
             alunos.append([nome, idade, nota])
 
         elif menu_escolha == 2:
-            for aluno in alunos:
-                print(f'Nome: {aluno[0]}')
-                print(f'Idade: {aluno[1]}')
-                print(f'Nota final: {aluno[2]}\n')
+            for indice, aluno in enumerate(alunos, start=1):
+                print(f"{indice}_ Nome: {aluno[0]} | Idade: {aluno[1]} | Nota: {aluno[2]}")
 
         elif menu_escolha == 3:
             nome_procurado = input('Digite o nome: ')
@@ -56,6 +63,14 @@ Sair: 5''')
                 print('aluno invalido!')
 
         elif menu_escolha == 5:
+            soma = 0
+            if len(alunos) == 0:
+                print('lista de alunos vazia, adicione alunos para que a media seja calculada')
+            else:
+                media = sum(aluno[2] for aluno in alunos) / len(alunos)
+                print(media)
+
+        elif menu_escolha == 6:
             print('Saindo do sistema')
             sair = True
 
