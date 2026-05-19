17432b24c9ad063ef37e261a8d1600f2e7241119
Luiz Gustavo Canzone Sikorski <canzoneluiz@gmail.com>
2026-05-19 14:23:18 -0300

    reformulada seleção de menu e o tratamento de entrada de dados.

--- main.py
+++ main.py
@@ -9,22 +9,52 @@ Remover aluno: 4
 media da turma: 5
 Sair: 6''')
 
+def menu_escolha():
+    while True:
+        try:
+            escolher = int(input('O que deseja fazer? '))
+
+            if escolher in [1, 2, 3, 4, 5, 6]:
+                return escolher
+
+            print('Escolha inválida!')
+
+        except ValueError:
+            print('Digite apenas números!')
+
 def cadastrar_aluno():
     while True:
         nome = input('Digite o nome do aluno: ').strip()
+
         if nome != "":
             break
-        print('Nome vazio!Tente novamente!')
+
+        print('Nome vazio! Tente novamente!')
+
     while True:
-        idade = int(input('Digite a idade: '))
-        if 0 < idade <= 100:
-            break
-        print('idade invalida!Tente novamente!')
+        try:
+            idade = int(input('Digite a idade: '))
+
+            if 0 < idade <= 100:
+                break
+
+            print('Idade inválida! Tente novamente!')
+
+        except ValueError:
+            print('Digite um valor numérico!')
+
     while True:
-        nota = float(input('Digite a nota final: '))
-        if 0 <= nota <= 10:
-            break
-        print('nota invalida!Tente novamente!')
+        try:
+            nota = float(input('Digite a nota final: '))
+
+            if 0 <= nota <= 10:
+                break
+
+            print('Nota inválida! Tente novamente!')
+
+        except ValueError:
+            print('Digite um valor numérico!')
+
     alunos.append([nome, idade, nota])
 
 def lista_alunos():
@@ -50,8 +80,8 @@ def remover_aluno():
             alunos.remove(aluno)
             print('Aluno removido!')
             break
-        else:
-            print('aluno invalido!')
+    else:
+        print('aluno invalido!')
 
 def media_alunos():
     if len(alunos) == 0:
@@ -66,30 +96,23 @@ while True:
 
     mostrar_menu()
 
-    try:
-        menu_escolha = int(input('O que deseja fazer? '))
+    escolha = menu_escolha()
 
-        if menu_escolha not in [1, 2, 3, 4, 5, 6]:
-            print('escolha inválida!')
+    if escolha == 1:
+        cadastrar_aluno()
 
-        elif menu_escolha == 1:
-            cadastrar_aluno()
+    elif escolha == 2:
+        lista_alunos()
 
-        elif menu_escolha == 2:
-            lista_alunos()
+    elif escolha == 3:
+        busca_aluno()
 
-        elif menu_escolha == 3:
-            busca_aluno()
+    elif escolha == 4:
+        remover_aluno()
 
-        elif menu_escolha == 4:
-            remover_aluno()
+    elif escolha == 5:
+        media_alunos()
 
-        elif menu_escolha == 5:
-            media_alunos()
-
-        elif menu_escolha == 6:
-            print('saindo do sistema')
-            break
-
-    except ValueError:
-        print('escolha invalida!')
+    elif escolha == 6:
+        print('saindo do sistema')
+        break
