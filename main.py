f1cfff1ab9822ca35a52d26514e09fd165bc9ceb
Luiz Gustavo Canzone Sikorski <canzoneluiz@gmail.com>
2026-05-19 13:34:01 -0300

    transformei as ações do menu em funções separadas
    melhorei a modularização do código para que ele fiquei mais organizado, profissional e fácil de arrumar em caso de problemas

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-sair = False
 alunos = []
 
-
-while sair == False:
-
+def mostrar_menu():
     print('''
 Cadastrar aluno: 1
 Listar alunos: 2
@@ -11,6 +8,64 @@ Buscar aluno: 3
 Remover aluno: 4
 media da turma: 5
 Sair: 6''')
+
+def cadastrar_aluno():
+    while True:
+        nome = input('Digite o nome do aluno: ').strip()
+        if nome != "":
+            break
+        print('Nome vazio!Tente novamente!')
+    while True:
+        idade = int(input('Digite a idade: '))
+        if 0 < idade <= 100:
+            break
+        print('idade invalida!Tente novamente!')
+    while True:
+        nota = float(input('Digite a nota final: '))
+        if 0 <= nota <= 10:
+            break
+        print('nota invalida!Tente novamente!')
+    alunos.append([nome, idade, nota])
+
+def lista_alunos():
+    for indice, aluno in enumerate(alunos, start=1):
+        print(f"{indice}_ Nome: {aluno[0]} | Idade: {aluno[1]} | Nota: {aluno[2]}")
+
+def busca_aluno():
+    nome_procurado = input('Digite o nome: ')
+    for aluno in alunos:
+        if aluno[0].lower() == nome_procurado.lower():
+            print(f'Nome: {aluno[0]}')
+            print(f'Idade: {aluno[1]}')
+            print(f'Nota final: {aluno[2]}')
+            break
+    else:
+        print('aluno invalido!')
+
+def remover_aluno():
+    nome_remover = input('Digite o nome do aluno: ')
+
+    for aluno in alunos:
+        if aluno[0].lower() == nome_remover.lower():
+            alunos.remove(aluno)
+            print('Aluno removido!')
+            break
+    else:
+        print('aluno invalido!')
+
+def media_alunos():
+    if len(alunos) == 0:
+        print('lista de alunos vazia, adicione alunos para que a media seja calculada')
+    else:
+        media = sum(aluno[2] for aluno in alunos) / len(alunos)
+        print(f"Média da turma: {media:.2f}")
+
+
+
+while True:
+
+    mostrar_menu()
+
     try:
         menu_escolha = int(input('O que deseja fazer? '))
 
@@ -18,61 +73,23 @@ Sair: 6''')
             print('escolha inválida!')
 
         elif menu_escolha == 1:
-            while True:
-                nome = input('Digite o nome do aluno: ').strip()
-                if nome != "":
-                    break
-                print('Nome vazio!Tente novamente!')
-            while True:
-                idade = int(input('Digite a idade: '))
-                if 0 < idade <= 100:
-                    break
-                print('idade invalida!Tente novamente!')
-            while True:
-                nota = float(input('Digite a nota final: '))
-                if 0 <= nota <= 10:
-                    break
-                print('nota invalida!Tente novamente!')
-            alunos.append([nome, idade, nota])
+            cadastrar_aluno()
 
         elif menu_escolha == 2:
-            for indice, aluno in enumerate(alunos, start=1):
-                print(f"{indice}_ Nome: {aluno[0]} | Idade: {aluno[1]} | Nota: {aluno[2]}")
+            lista_alunos()
 
         elif menu_escolha == 3:
-            nome_procurado = input('Digite o nome: ')
-
-            for aluno in alunos:
-                if aluno[0].lower() == nome_procurado.lower():
-                    print(f'Nome: {aluno[0]}')
-                    print(f'Idade: {aluno[1]}')
-                    print(f'Nota final: {aluno[2]}')
-                    break
-            else:
-                print('aluno invalido!')
+            busca_aluno()
 
         elif menu_escolha == 4:
-            nome_remover = input('Digite o nome do aluno: ')
-
-            for aluno in alunos:
-                if aluno[0].lower() == nome_remover.lower():
-                    alunos.remove(aluno)
-                    print('Aluno removido!')
-                    break
-            else:
-                print('aluno invalido!')
+            remover_aluno()
 
         elif menu_escolha == 5:
-            soma = 0
-            if len(alunos) == 0:
-                print('lista de alunos vazia, adicione alunos para que a media seja calculada')
-            else:
-                media = sum(aluno[2] for aluno in alunos) / len(alunos)
-                print(media)
+            media_alunos()
 
         elif menu_escolha == 6:
-            print('Saindo do sistema')
-            sair = True
+            print('saindo do sistema')
+            break
 
     except ValueError:
         print('escolha invalida!')
